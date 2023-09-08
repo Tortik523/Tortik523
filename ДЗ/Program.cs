db955cacd17099ba3160c35b56865f59180aa85b
Tortik523 <deadtortik5@gmail.com>
2023-09-08 20:55:14 +0200

    Добавьте файлы проекта.

---
+++ ДЗ/Program.cs
@@ -0,0 +1,29 @@
+﻿////13
+//Console.WriteLine("Введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Результат");
+//double E = Math.Sqrt(Math.Abs(3 * y * y + 0.5 * y + 4));
+//Console.WriteLine($"{E}");
+
+////17
+//Console.WriteLine("введите y:");
+//double y= double.Parse(Console.ReadLine());
+//Console.WriteLine("Решение");
+//double Z = 3 * y * y + Math.Sqrt(y + 1);
+//Console.WriteLine($"Z={Z}");
+
+
+
+
+//Console.WriteLine("Дано четырехзначное число. Найти: а) сумму его цифр; б) произведение его цифр.");
+//Console.Write("Введите n:");
+//int n = int.Parse(Console.ReadLine());
+//int d = n % 10;
+//int c = n % 100 / 10;
+//int b = n % 1000 / 100;
+//int a = n / 1000;
+
+//Console.Write("а) Сумма цифр введенного числа равна:");
+//Console.WriteLine(a + b + c + d);
+//Console.Write("б) Произведение цифр введенного числа равна:");
+//Console.WriteLine(a * b * c * d);
